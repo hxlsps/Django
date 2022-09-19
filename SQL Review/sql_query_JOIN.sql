1d089b8245ff88906123a0b727335842be8a35a6
hxlsps <xuanloc11042000@gmail.com>
2022-09-18 22:58:50 -0500

    SQL

--- SQL Review/sql_query_JOIN.sql
+++ SQL Review/sql_query_JOIN.sql
@@ -1,22 +1,34 @@
 -- E. SQLite Joins
 -- 1. Write a query to find the addresses (location_id, street_address, city, state_province, country_name) of all the departments. 
-
+--SELECT d.department_id, d.location_id, l.street_address, l.city, l.state_province, c.country_name FROM departments d LEFT JOIN locations l
+--ON d.location_id = l.location_id
+--LEFT JOIN countries c 
+--ON l.country_id = c.country_id;
 -- 2. Write a query to find the names (first_name, last name), department ID and the name of all the employees.
-
+--SELECT first_name, last_name, department_id FROM employees;
 -- 3. Write a query to find the employee id, name (last_name) along with their manager_id, manager name (last_name).
 
 -- 4. Write a query to find the names (first_name, last_name) and hire date of the employees who were hired after 'Jones'.
-
--- 5. Write a query to get the department name and number of employees in the department.
-
--- 6. Write a query to find the employee ID, job title number of days between ending date and starting date for all jobs in department 90 from job history.
-
+--SELECT first_name, last_name, hire_date FROM employees WHERE hire_date > (SELECT hire_date FROM employees WHERE last_name = 'Jones' LIMIT 1);
+-- LUU Y 5. Write a query to get the department name and number of employees in the department.
+--SELECT d.department_id, d.department_name, count(*) FROM employees e LEFT JOIN department d 
+--ON e.department_id  = d.department_id
+--GROUP BY e.department_id ;
+-- CHI HIEN NAM 6. Write a query to find the employee ID, job title number of days between ending date and starting date for all jobs in department 90 from job history.
+--SELECT jh.employee_id, j.job_title, jh.end_date - jh.start_date FROM job_history jh LEFT JOIN jobs j
+--ON jh.job_id = j.job_id
+--WHERE jh.department_id = 90;
 -- 7. Write a query to display the department ID, department name, and manager first name. 
 
 -- 8. Write a query to display the department name, manager name, and city. 
 
 -- 9. Write a query to display the job title and average salary of employees.
-
+--SELECT last_name, j.job_title, e.Avg_Salary FROM employees e LEFT JOIN jobs j 
+--ON e.job_id = j.job_id;
 -- 10. Write a query to to display job title, employee name, and the difference between the salary of the employee and minimum salary for the job.
-
+-- SELECT e.last_name||' '||e.first_name AS 'Name', j.job_title, e.salary - j.min_salary  FROM employees e LEFT JOIN jobs j 
+-- ON e.job_id = j.job_id;
 -- 11. Write a query to display the job history that was done by any employee who is currently drawing more than 10000 of salary.
+SELECT jh.* FROM employees e JOIN job_history jh 
+ON e.job_id = jh.job_id 
+WHERE e.salary > 10000;
--- SQL Review/sql_query_SUBQUERIES.sql
+++ SQL Review/sql_query_SUBQUERIES.sql
@@ -2,37 +2,38 @@
 -- D. SQLite Subquery
 
 -- 1. Write a query to find the names (first_name, last_name) and salaries of the employees who have a higher salary than the employee whose last_name='Bull'. 
-
+-- SELECT first_name, last_name, salary FROM employees WHERE salary > (SELECT salary FROM employees WHERE last_name  = 'Bull');
 -- 2. Write a query to find the names (first_name, last_name) of all employees who works in the IT department.
-
+-- SELECT first_name, last_name FROM employees WHERE department_id = (SELECT department_id FROM department WHERE department_name LIKE '%IT%');
 -- 3. Write a query to find the names (first_name, last_name) of the employees who have a manager who works for a department based in the United States.
 
 -- 4. Write a query to find the names (first_name, last_name) of the employees who are managers.
 
 -- 5. Write a query to find the names (first_name, last_name), the salary of the employees whose salary is greater than the average salary.
-
+-- SELECT last_name, first_name, salary FROM employees WHERE salary > (SELECT AVG(salary) FROM employees);
 -- 6. Write a query to find the names (first_name, last_name), the salary of the employees whose salary is equal to the minimum salary for their job grade. 
-
+--SELECT first_name, last_name, job_id, salary FROM employees e1 WHERE salary IN (SELECT min_salary FROM jobs WHERE job_id = e1.job_id);
 -- 7. Write a query to find the names (first_name, last_name), the salary of the employees who earn more than the average salary and who works in any of the IT departments. 
-
+-- SELECT first_name, last_name, salary FROM employees WHERE salary > (SELECT AVG(salary) FROM employees) 
+-- AND salary >= (SELECT MAX(salary) FROM employees WHERE department_id = (SELECT department_id FROM department WHERE department_name LIKE '%IT%')); 
 -- 8. Write a query to find the names (first_name, last_name), the salary of the employees who earn more than Mr. Bell.
-
+--SELECT first_name, last_name, salary FROM employees WHERE salary >= (SELECT MAX(salary) FROM employees WHERE last_name = 'Bell' OR first_name = 'Bell');
 -- 9. Write a query to find the names (first_name, last_name), the salary of the employees who earn the same salary as the minimum salary for all departments.
-
+-- SELECT first_name, last_name, salary FROM employees WHERE salary = (SELECT min_salary FROM jobs);
 -- 10. Write a query to find the names (first_name, last_name) of the employees who are not supervisors.
 
 -- 11. Write a query to display the employee ID, first name, last names, salary of all employees whose salary is above average for their departments.
+-- SELECT employee_id, first_name, last_name FROM employees e1 WHERE salary > (SELECT AVG(salary) FROM employees e2 GROUP BY department_id HAVING e2.department_id=e1.department_id);
+-- 12. (SOS) Write a query to find the 5th maximum salary in the employees table.
 
--- 12. Write a query to find the 5th maximum salary in the employees table.
+-- 13. (SOS) Write a query to find the 4th minimum salary in the employees table.
 
--- 13. Write a query to find the 4th minimum salary in the employees table.
-
--- 14. Write a query to select last 10 records from a table.
+-- 14.(SOS) Write a query to select last 10 records from a table.
 
 -- 15. Write a query to list department number, name for all the departments in which there are no employees in the department.
 
--- 16. Write a query to get 3 maximum salaries. 
+-- 16. (SOS) Write a query to get 3 maximum salaries. 
 
--- 17. Write a query to get 3 minimum salaries.
+-- 17. (SOS) Write a query to get 3 minimum salaries.
 
--- 18. Write a query to get nth max salaries of employees.
+-- 18. (SOS) Write a query to get nth max salaries of employees.
